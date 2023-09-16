a1e93b85955b0538e57d1cd8642d9dba08dbdd5c
Chen Hua-En <chenhuaen171@Chens-MacBook-Pro.local>
2023-09-16 23:35:00 +0800

    First Commit with Sign Up, Login, Components and App.js

---
+++ src/Components/Navbar/NavbarComp.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Navbar, Nav, Container, Form, Button, NavDropdown} from 'react-bootstrap';
+import './NavbarComp.css'
+
+
+export const NavbarComp = ({onRouteChange}) => {
+
+  return (
+    <Navbar bg="black fixed-top" variant="dark" className='navbar'>
+      <Container>
+        <Navbar.Brand href="#home" className='Company'>TIX</Navbar.Brand>
+  
+        <Form className="d-flex">
+          <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search"/>
+          <Button variant="outline-success" className='search-button'>Search</Button>
+        </Form>
+
+        
+        <Nav className="ms-auto underline-on-hover">
+          <Nav.Link onClick={() => {onRouteChange('AllEvents')}} href="#home">Home</Nav.Link>
+
+          <NavDropdown title="Events" id="basic-nav-dropdown">
+            <NavDropdown.Item 
+            onClick={() => {onRouteChange('AllEvents')}}
+            className="dropdownitem" href="#events-all">All</NavDropdown.Item>
+            <NavDropdown.Item 
+            onClick={() => {onRouteChange('Music')}}
+            className="dropdownitem" href="#events-music">Music</NavDropdown.Item>
+            <NavDropdown.Item  
+            onClick={() => {onRouteChange('Sports')}}
+            className="dropdownitem" href="#events-sports">Sports</NavDropdown.Item>
+          </NavDropdown>
+
+          <Nav.Link href="#FAQ">FAQ</Nav.Link>
+          <Nav.Link 
+          onClick={() => {onRouteChange('Login')}}
+          href="#login">Login</Nav.Link>
+          <Nav.Link 
+          onClick={() => {onRouteChange('SignUp')}}
+          href="#signup">Sign Up</Nav.Link>
+          </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarComp;
---
+++ src/Components/Navbar/NavbarCompProfile.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown, Form, Button } from 'react-bootstrap';
+import './NavbarComp.css'
+
+export const NavbarCompProfile = ({loadUser, onRouteChange}) => {
+  return (
+    <Navbar bg="black fixed-top" variant="dark">
+      <Container>
+        
+        <Form className="d-flex">
+          <Form.Control type="search" placeholder="Search" className="me-2 search-input" aria-label="Search"/>
+          <Button variant="outline-success" className='search-button'>Search</Button>
+        </Form>
+
+        <Navbar.Brand href="#home" className="logo">TIX</Navbar.Brand>
+        <Nav className="ms-auto underline-on-hover">
+          <Nav.Link onClick={() => {onRouteChange('AllEvents')}} href="#home">Home</Nav.Link>
+          <NavDropdown title="Events" id="basic-nav-dropdown">
+            <NavDropdown.Item 
+            onClick={() => {onRouteChange('AllEvents')}}
+            className="dropdownitem" href="#events-all">All</NavDropdown.Item>
+            <NavDropdown.Item 
+            onClick={() => {onRouteChange('Music')}}
+            className="dropdownitem" href="#events-music">Music</NavDropdown.Item>
+            <NavDropdown.Item  
+            onClick={() => {onRouteChange('Sports')}}
+            className="dropdownitem" href="#events-sports">Sports</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#FAQ">FAQ</Nav.Link>
+        </Nav>
+
+        <Nav className="ml-auto">
+        <NavDropdown title="Profile" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#profile">Profile</NavDropdown.Item>
+          <NavDropdown.Item 
+          onClick={() => {loadUser('')}}
+          href="#signout">Sign Out</NavDropdown.Item>
+        </NavDropdown>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarCompProfile;
