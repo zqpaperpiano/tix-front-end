ec58615deb75875e09505bedb9d10ef6426c1a0f
zqpaperpiano <ziqing.ong.2021@socsc.smu.edu.sg>
2023-09-22 13:56:34 +0800

    created homepage

--- src/Events/EventsAll.jsx
+++ src/Events/EventsAll.jsx
@@ -3,9 +3,7 @@ import './EventsAll.css'
 import { Button } from 'react-bootstrap';
 import TaylorSwift from '../assets/TaylorSwift.jpeg' 
 import UFC from '../assets/UFC.jpeg'
-import UFC2 from '../assets/UFC_resized.jpeg'
 import { Carousel } from 'react-responsive-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css'
 import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export const EventsAll = ({onRouteChange}) => {
@@ -16,24 +14,6 @@ export const EventsAll = ({onRouteChange}) => {
           <Button variant="light" className="allbutton" href="#EventsAll">All</Button>
           <Button onClick={() => onRouteChange('Music')} variant="light" className="musicbutton" href="#EventsMusic">Music</Button>
           <Button onClick={() => onRouteChange('Sports')} variant="light" className="sportbutton" href="#EventsSports">Sports</Button>
-        </div>
-
-        <div className="carousel">
-          <Carousel className="carousel-format" 
-          autoPlay={true}
-          infiniteLoop={true}
-          showThumbs={false}
-          showArrows={true}
-          width={1100}>
-            <div>
-              <img onClick={() => {onRouteChange('TSInfo')}} className="taylor-2" alt="Taylor swift" src={TaylorSwift} />
-            </div>
-
-            <div>
-              <img className="UFC-2" alt="Ufc" src={UFC2} />
-            </div>
-
-          </Carousel>
         </div>
         
       <div className="events-overview">
