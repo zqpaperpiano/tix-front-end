7ee4b9b9dac4ed9a471c496aba268dcbf9657d20
zqpaperpiano <ziqing.ong.2021@socsc.smu.edu.sg>
2023-09-23 17:23:59 +0800

    added confirmation box during payment

--- src/BuyingPage/Payment.jsx
+++ src/BuyingPage/Payment.jsx
@@ -7,43 +7,75 @@ export const Payment = () => {
 
   return (
     <div className="payment">
-      <div className="div">
 
-        {/* Need to add a table with confirmation details */}
-
-        <div className="text-wrapper">Payment</div>
-        <div className="credit-card-number">
-          <div className="text-wrapper-2">Credit Card Number</div>
-          <div className="overlap-group">
-            <input className="number" />
+        <div className="confirmation-title">
+          <img className="taylor-swift" alt="Taylor swift" src={TaylorSwitft} />
+          <div className="taylor-swift-title">
+            <p className="text-wrapper-2">Taylor Swift Era Tour</p>
+            <p className="text-wrapper-2">2 March 2024 (Sat) ~ 9 March 2024 (Sat)</p>
+            <p className="text-wrapper-5">Singapore National Stadium</p>
           </div>
         </div>
 
-        <div className="dateof-expiry">
-          <div className="text-wrapper-3">Date of Expiry</div>
-          <div className="overlap">
-            <input className="date" />
+        <div className="confirmation-details">
+          <div className="confirm-date">
+            <p>Date</p>
+          </div>
+
+          <div className="confirm-time">
+            <p>Time</p>
+          </div>
+
+          <div className="confirm-name">
+            <p>Name</p>
+          </div>
+
+          <div className="confirm-cat">
+            <p>Category</p>
+          </div>
+
+          <div className="confirm-price">
+            <p>Price</p>
+          </div>
+
+          <div className="confirm-seat">
+            <p>Seat Number:</p>
           </div>
         </div>
 
-        <div className="CVV">
-          <div className="text-wrapper-3">CVV</div>
-          <div className="CVV-wrapper">
-            <input className="input" />
+        <div className="card-details-payment">
+
+        <p className="text-wrapper-5">Payment</p>
+
+        <div className="card-details">
+          <div className="credit-card-number">
+            <p className="text-wrapper-2">Credit Card Number</p>
+            <div className="card-input-wrapper">
+              <input className="number" />
+            </div>
+          </div>
+
+          <div className="dateof-expiry">
+            <p className="text-wrapper-2">Date of Expiry</p>
+            <div className="card-input-wrapper">
+              <input className="date" />
+            </div>
+          </div>
+
+          <div className="CVV">
+            <p className="text-wrapper-2">CVV</p>
+            <div className="card-input-wrapper">
+              <input className="input" />
+            </div>
           </div>
         </div>
+        
+        <div className="payment-placement text-wrapper-2">
+          <button className="payment-button">Complete Payment</button>
+        </div> 
 
-        <img className="taylor-swift" alt="Taylor swift" src={TaylorSwitft} />
-        <div className="taylor-swift-title">
-          <div className="text-wrapper-4">Taylor Swift Era Tour</div>
-          <p className="p">2 March 2024 (Sat) ~ 9 March 2024 (Sat)</p>
-          <div className="text-wrapper-5">Singapore National Stadium</div>
         </div>
 
-        <div className="text-wrapper-6">
-          <button>Complete Payment</button>
-        </div> {/* Finish Button */}
-      </div>
     </div>
   );
 };
